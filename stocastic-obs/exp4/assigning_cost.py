d9caa15c312566ed488f5a75c6e75242ae4388a5
Lokesh <boominathanlokesh@gmail.com>
2023-04-17 17:30:24 -0500

    minor update

--- stocastic-obs/exp4/assigning_cost.py
+++ stocastic-obs/exp4/assigning_cost.py
@@ -8,7 +8,7 @@ def plot_exp_settings(exp_params, no_attention_modes = 6):
     attention_cost_coeff, attention_cost_temp = exp_params
     obs_certainity_possible = 1/(2*(no_attention_modes-1)) * np.arange(no_attention_modes) + 0.5
     attention_cost = np.array([-attention_cost_coeff * np.exp(certainity/attention_cost_temp) for certainity in obs_certainity_possible])
-    plt.plot(obs_certainity_possible, attention_cost, 'o-')
+    plt.plot(obs_certainity_possible, attention_cost - attention_cost[0], 'o-')
 exp_name_list = []
 for exp_name in exps_list.keys():
     exp_name_list.append(str(exp_name))
