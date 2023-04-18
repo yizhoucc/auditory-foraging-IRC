f9d545d8360f790d310fd3be8fd5a86c45bc62d7
Lokesh <boominathanlokesh@gmail.com>
2023-04-18 10:55:31 -0500

    more exps

--- stocastic-obs/exp4/assigning_cost.py
+++ stocastic-obs/exp4/assigning_cost.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # each element is of the form {exp_name: (attention_coefficient, attention_temperature)}
-exps_list = {0: (25, 10), 2: (38, 15), 3: (50, 20), 4: (63, 25), 6: (13, 5), 7: (13, .75), 8: (13, 1), 9: (13, .65), 10: (13, .5)}
+exps_list = {0: (25, 10), 2: (38, 15), 3: (50, 20), 4: (63, 25), 6: (13, 5), 7: (13, .75), 8: (13, 1), 9: (13, .65), 10: (13, .5), 11: (.05, .25), 12: (.06, .25), 13: (.07, .25), 14: (.08, .25)}
+
+selected_exps = exps_list.keys()
+selected_exps = [0,2,3,4,6, 11, 12, 13, 14]
 
 def plot_exp_settings(exp_params, no_attention_modes = 6):
     attention_cost_coeff, attention_cost_temp = exp_params
@@ -10,7 +13,7 @@ def plot_exp_settings(exp_params, no_attention_modes = 6):
     attention_cost = np.array([-attention_cost_coeff * np.exp(certainity/attention_cost_temp) for certainity in obs_certainity_possible])
     plt.plot(obs_certainity_possible, attention_cost - attention_cost[0], 'o-')
 exp_name_list = []
-for exp_name in exps_list.keys():
+for exp_name in selected_exps:
     exp_name_list.append(str(exp_name))
     plot_exp_settings(exps_list[exp_name])
 plt.xlabel('certainity')
