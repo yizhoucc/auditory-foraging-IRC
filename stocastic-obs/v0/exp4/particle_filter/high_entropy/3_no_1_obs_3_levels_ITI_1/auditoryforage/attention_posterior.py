d2143eb93b51fbe77eb547d585947cf022b017e3
Lokesh <boominathanlokesh@gmail.com>
2023-07-21 15:43:09 -0500

    updates

--- stocastic-obs/v0/exp4/particle_filter/high_entropy/3_no_1_obs_3_levels_ITI_1/auditoryforage/attention_posterior.py
+++ stocastic-obs/v0/exp4/particle_filter/high_entropy/3_no_1_obs_3_levels_ITI_1/auditoryforage/attention_posterior.py
@@ -6,13 +6,15 @@ class AttentionPosterior():
     def __init__(self, agent, env, episode, end_index):
         self.agent = agent
         self.env = env
-        self.state_list, self.lick_actions = self.extract_data(episode, end_index)
-        self.observation_matrix = env.find_observation_matrix
-        self.transition_matrix = env.find_transition_matrix
+        self.end_index = end_index
+        self.state_list, self.lick_actions = self.extract_data(episode)
+        self.observation_matrix = env.find_observation_matrix()
+        self.transition_matrix = env.find_transition_matrix()
         self.attention_posterior_IO = {}
+        self.attention_posterior_IO['input'] = {}
         self.attention_posterior_IO['input']['root_episode'] = episode
         
-    def extract_data(self, episode, end_index):
+    def extract_data(self, episode):
         state_list = [list_of_state[0] for list_of_state in episode['states']]
         lick_actions = [self.env.dict_action_possible[action][0] for action in episode['actions']]
         state_list = state_list[:self.end_index]
@@ -38,7 +40,7 @@ class AttentionPosterior():
         return [key for key, val in self.env.dict_action_possible.items() if val == (lick, attention)][0]
     
     def compute_joint(self, obs_attention_series):
-        def output_relevant_variables(self, time):
+        def output_relevant_variables(time):
             prev_lick = self.lick_actions[time-1] if time != 0 else None
             prev_attention = obs_attention_series[time-1][1] if time != 0 else None
             prev_action = [key for key, val in self.env.dict_action_possible.items() if val == (prev_lick, prev_attention)][0] if time != 0 else None
@@ -47,19 +49,23 @@ class AttentionPosterior():
             current_obs = obs_attention_series[time][0]
             current_lick = self.lick_actions[time]
             current_attention = obs_attention_series[time][1]
-            current_action = self.find_action_key(self, current_lick, current_attention)
+            current_action = self.find_action_key(current_lick, current_attention)
             return prev_lick, prev_attention, prev_action, prev_state, current_state, current_obs, current_action
         _, _, _, _, current_state, current_obs, current_action = output_relevant_variables(0)
-        belief = self.env.init_belief(current_obs)
-        action_probs = self.agent.agent_action_distribution(belief)[0]
-        joint_prob = self.observation_matrix(current_obs, current_state, 0) * action_probs[current_action]
+        belief = self.env.init_belief([current_obs])
+        action_probs = self.agent.agent_action_distribution(np.array([belief]))[0]
+        joint_prob = self.observation_matrix[current_obs, current_state, 0] * action_probs[current_action]
         for time in range(1, len(obs_attention_series)):
             prev_lick, prev_attention, prev_action, prev_state, current_state, current_obs, current_action = output_relevant_variables(time)
-            joint_prob *= self.transition_matrix(prev_state, current_state, prev_lick)
-            joint_prob *= self.observation_matrix(current_obs, current_state, prev_attention)
-            belief = self.env.update_belief(self, belief, prev_action, current_obs)
-            action_probs = self.agent.agent_action_distribution(belief)[0]
-            joint_prob *= action_probs[current_action]
+            joint_prob *= self.transition_matrix[prev_state, current_state, prev_lick]
+            joint_prob *= self.observation_matrix[current_obs, current_state, prev_attention]
+            belief = self.env.update_belief(belief, prev_action, current_obs)
+            if belief is not None:
+                action_probs = self.agent.agent_action_distribution(np.array([belief]))[0]
+                joint_prob *= action_probs[current_action]
+            else:
+                joint_prob = 0
+                break
         return joint_prob
     
     def marginalize(self, series_samples):
@@ -89,10 +95,15 @@ class AttentionPosterior():
         for attention_series in attention_series_list:
             posterior_list.append(self.compute_posterior_for_given_attention_series(attention_series))
         self.agent.algo.policy.set_training_mode(_to_restore_train)
-        sorted_indices = np.argsort(-1 * np.array(posterior_list))
-        attention_series_list = attention_series_list[sorted_indices]
-        posterior_list = posterior_list[sorted_indices]
+        
+        print(posterior_list)
+        sorted_indices = list(np.argsort(-1 * np.array(posterior_list)))
+        print(sorted_indices)
+
+        attention_series_list = [attention_series_list[ind] for ind in sorted_indices]
+        posterior_list = [posterior_list[ind] for ind in sorted_indices]
+        self.attention_posterior_IO['output'] = {}
         self.attention_posterior_IO['output']['attention_series_list'] = attention_series_list
         self.attention_posterior_IO['output']['posterior_list'] = posterior_list
-        if do_save: self.save_output(self.attention_posterior_IO)
+        if do_save: self.save_output()
         return self.attention_posterior_IO
