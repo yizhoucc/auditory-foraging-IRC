860376dc00cff7ffb9b7826c3948a5aace2c769a
Lokesh Boominathan <lokeshboominathan@Lokeshs-MacBook-Pro.local>
2023-10-10 14:58:20 -0500

    m2c

--- stocastic-obs/v0/exp4/particle_filter/high_entropy/5_no_1_obs_2_levels_ITI_1_trial_1/attention_control/ppo/multiple_seeds/att_cost_-2.847/ent_0.01_gamma_1/train_14.py
+++ stocastic-obs/v0/exp4/particle_filter/high_entropy/5_no_1_obs_2_levels_ITI_1_trial_1/attention_control/ppo/multiple_seeds/att_cost_-2.847/ent_0.01_gamma_1/train_14.py
@@ -14,7 +14,7 @@ manager = IRCManager(defaults=defaults)
 
 ##############################################################################
 
-env_param = [0, 25.0, .08, .25, 0, 0]
+env_param = [0, 20, .08, .25, 0, 0]
 
 # change back to old no. of epochs
 num_epochs = 100
@@ -22,5 +22,4 @@ num_epochs = 100
 for seed in range(5):
     print(f'running seed {seed}')
     agent = manager.train_agent(env_param, num_epochs=num_epochs, seed = seed)
-
 
