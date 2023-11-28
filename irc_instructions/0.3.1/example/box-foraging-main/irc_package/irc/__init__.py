07b0f7a31cdaf505fec8b454c82d355af96980e5
Lokesh Boominathan <32840514+Lokesh-Boominathan@users.noreply.github.com>
2023-11-28 11:23:59 -0500

    Delete irc_instructions directory

--- irc_instructions/0.3.1/example/box-foraging-main/irc_package/irc/__init__.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import yaml
-from jarvis.config import Config
-
-with open(Path(__file__).parent/'VERSION.txt', 'r') as f:
-    __version__ = f.readline().split('"')[1]
-with open(Path(__file__).parent/'rcParams.yaml', 'r') as f:
-    rcParams: Config = Config(yaml.safe_load(f))
